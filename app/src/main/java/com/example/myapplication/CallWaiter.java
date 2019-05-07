a2773394112da351842728016900383eb3b34227
DrKruZz <valentin.colliard@gmail.com>
2019-05-07 18:58:52 +0200

    iji

--- app/src/main/java/com/example/myapplication/CallWaiter.java
+++ app/src/main/java/com/example/myapplication/CallWaiter.java
@@ -1,13 +1,44 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CallWaiter extends AppCompatActivity {
 
+    private Button yesButton;
+    private Button noButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_waiter);
+
+        yesButton = (Button) findViewById(R.id.btnDialogYes);
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWaiterComingActivity();
+            }
+        });
+
+        noButton = (Button) findViewById(R.id.btnDialogNo);
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMenuActivity();
+            }
+        });
+    }
+
+    public void openWaiterComingActivity(){
+        Intent intent = new Intent(this, WaiterComingActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMenuActivity(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/WaiterComingActivity.java
+++ app/src/main/java/com/example/myapplication/WaiterComingActivity.java
@@ -1,13 +1,27 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WaiterComingActivity extends AppCompatActivity {
+
+    private static int TIME_OUT = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiter_coming);
+        final View myLayout = findViewById(R.id.lv_menu);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(WaiterComingActivity.this, MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },TIME_OUT);
     }
 }
